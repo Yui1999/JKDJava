9e73f96e842fe1d9a7105bd3242c1a3dcf167b9c
Yui1999 <2083235753@qq.com>
2020-11-19 09:42:12 +0800

    JDBC Connection

--- JKDdemo/src/com/company/jdbcDemo1.java
+++ JKDdemo/src/com/company/jdbcDemo1.java
@@ -1,13 +1,10 @@
 package com.company;
 
-import java.sql.Statement;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.Statement;
 
-/**
- * JDBC链接mysql示例
- */
-public class JDBC {
+public class jdbcDemo1 {
     public static void main(String[] args) throws Exception{
         //1、导入驱动jar包, 建立libs文件夹， 右键 add as library
         //2、 注册驱动
@@ -16,16 +13,16 @@ public class JDBC {
         Connection conn = DriverManager.getConnection(
                 "jdbc:mysql://"+"localhost:3306/jkdjava","root","123456");
         //4、定义sql
-        String sql = "update account set balance=500 where id = 1";
+        String sql = "insert into account values(null,'城钱伦',250)";
         //5、获取数据库连接对象statement
         Statement stmt = conn.createStatement();
         //6、执行sql
         int i = stmt.executeUpdate(sql);
         //7、处理结果
         if(i==1){
-            System.out.println("更新成功!");
+            System.out.println("插入成功!");
         }else{
-            System.out.println("更新失败!");
+            System.out.println("插入失败!");
         }
         //8、释放资源
         stmt.close();
--- JKDdemo/src/com/company/jdbcDemo3.java
+++ JKDdemo/src/com/company/jdbcDemo3.java
@@ -1,13 +1,11 @@
 package com.company;
 
-import java.sql.Statement;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
-/**
- * JDBC链接mysql示例
- */
-public class JDBC {
+public class jdbcDemo3 {
     public static void main(String[] args) throws Exception{
         //1、导入驱动jar包, 建立libs文件夹， 右键 add as library
         //2、 注册驱动
@@ -16,19 +14,21 @@ public class JDBC {
         Connection conn = DriverManager.getConnection(
                 "jdbc:mysql://"+"localhost:3306/jkdjava","root","123456");
         //4、定义sql
-        String sql = "update account set balance=500 where id = 1";
+        String sql = "select * from account";
         //5、获取数据库连接对象statement
         Statement stmt = conn.createStatement();
         //6、执行sql
-        int i = stmt.executeUpdate(sql);
+        ResultSet rs = stmt.executeQuery(sql);
         //7、处理结果
-        if(i==1){
-            System.out.println("更新成功!");
-        }else{
-            System.out.println("更新失败!");
+        while(rs.next()){
+            int id = rs.getInt("id");
+            String name = rs.getString("name");
+            double balance = rs.getDouble("balance");
+            System.out.println(id+"----"+name+"----"+balance);
         }
         //8、释放资源
         stmt.close();
         conn.close();
     }
+
 }
