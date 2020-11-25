5bf25c3d7ac61c472b68854ce7e379f4e9df7d65
Yui1999 <2083235753@qq.com>
2020-11-25 11:30:32 +0800

    23456

--- redbag/src/main/java/com/neusoft/redbag/LuckyMoneyController.java
+++ redbag/src/main/java/com/neusoft/redbag/LuckyMoneyController.java
@@ -4,8 +4,10 @@ package com.neusoft.redbag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 //相当于@Controller 和 @ResponseBody
@@ -25,5 +27,19 @@ public class LuckyMoneyController {
     @RequestMapping("/list")
     public List<LuckyMoney> list(){
         return repository.findAll();
+    }
+
+    /**
+     * 发送红包
+     * @param producer
+     * @param money
+     * @return
+     */
+    @RequestMapping("/post")
+    public LuckyMoney postRedBag(@RequestParam(value = "producer",required = true)String producer,@RequestParam(value = "money",required = true)BigDecimal money){
+       LuckyMoney luckyMoney = new LuckyMoney();
+       luckyMoney.setProducer(producer);
+       luckyMoney.setMoney(money);
+       return repository.save(luckyMoney);
     }
 }
